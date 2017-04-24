21412e373c9ccc72f72959ec209bbfb34a47603c
sofiasartori <sofiainessartori@hotmail.com>
2017-04-24 17:36:00 -0300

    todavia no funciona el array

--- fabrica.php
+++ fabrica.php
@@ -6,16 +6,24 @@
 
 		function __construct($_razonSocial){
 			$this->_empleados = array();
-			$this->_razonSocial = $_razonSocial;
+			$this->_razonSocial = $_razonSocial;			
 		}
 
-		static function AgregarEmpleado ($persona){
-
+		function AgregarEmpleado ($persona){
+			echo var_dump($this->_empleados);
 			array_push($_empleados, $persona);
 		}
 
 		static function CalcularSueldos(){
+			$sueldos = 0;
 
+			for ($i=0; $i < count($_empleados)	; $i++)	 { 
+				$sueldos += array_column($_empleados, 'sueldo');
+			}
+			/*foreach ($_empleados as $sueldo) {
+				$sueldos += $sueldo;
+			}*/
+			echo $sueldos;
 		}
 
 		static function EliminarEmpleado($persona){
